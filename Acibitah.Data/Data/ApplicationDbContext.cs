da573feaaddbda79b798853b68ea2fa4f24e49c4
klaudiaRajs <klaudia.rajs@gmail.com>
2023-08-08 10:05:13 +0200

    Add project files.

---
+++ Acibitah.Data/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Acibitah.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Acibitah.Data.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ToDoTask> ToDoTasks { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ToDoTask>().HasData(
+                new ToDoTask() { Id = 1, Title = "Action", Content = "Test" }
+            );
+        }
+
+    }
+}
---
+++ Acibitah.Models/Enums/Difficulty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acibitah.Models.Enums
+{
+    public enum Difficulty
+    {
+        Easy = 0,
+        Medium = 1, 
+        Hard = 2, 
+        Master = 3
+    }
+}
