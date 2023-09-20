89189a1a813c333ec9407b153318640589147e1c
klaudiaRajs <klaudia.rajs@gmail.com>
2023-09-20 14:31:22 +0200

    Moving error messages into consts

--- Acibitah/Controllers/TaskController.cs
+++ Acibitah/Controllers/TaskController.cs
@@ -1,12 +1,20 @@
 ﻿using Acibitah.Data.Repositories.Interfaces;
 using Acibitah.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Data.SqlClient.DataClassification;
 
 namespace Acibitah.Controllers
 {
     public class TaskController : Controller
     {
+        public const string TASK_NOT_FOUND = "Task not found, please try again later.";
+        public const string NEW_TASK_CREATED = "New task created.";
+        public const string TASK_NOT_CREATED = "Problem with creating the task. Try fixing some data or try again later.";
+        public const string KEY_ERROR_MESSAGE = "ErrorMessage";
+        public const string KEY_SUCCESS_MESSAGE = "SuccessMessage";
+        public const string SUCCESSFULLY_DELETED = "Deleted.";
+        public const string REMOVING_PROBLEM = "Problem with removing task. Try again later. ";
+        public const string ERROR_TASK_NOT_SAVED = "Problem with saving the task"; 
+
         private ITaskRepository _taskRepository;
         private TaskViewModel _taskViewModel;
         public TaskController(ITaskRepository taskRepository)
@@ -23,28 +31,79 @@ namespace Acibitah.Controllers
             return View(_taskViewModel);
         }
 
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            _taskViewModel.ToDo = _taskRepository.GetById(id);
+            if( _taskViewModel.ToDo == null)
+            {
+                TempData[KEY_ERROR_MESSAGE] = TASK_NOT_FOUND;
+                return RedirectToAction("Index");
+            }
+
+            _taskViewModel.Subtasks = _taskRepository.GetSubtasksByTask(_taskViewModel.ToDo);
+            return View(_taskViewModel); 
+        }
+
         [HttpPost]
         public IActionResult Index(ToDoTask task)
         {
-            //TODO Opracuj result
             var result = _taskRepository.Save(task);
-            return RedirectToAction("Index");
+            if( result) 
+            {
+                TempData[KEY_SUCCESS_MESSAGE] = NEW_TASK_CREATED;
+                return RedirectToAction("Index");
+            } else
+            {
+                TempData[KEY_ERROR_MESSAGE] = TASK_NOT_CREATED;
+                _taskViewModel.ToDo = task; 
+                return View(_taskViewModel);
+            }
         }
         public IActionResult Remove(int id)
         {
-            //TODO opracuj result
-            var result = _taskRepository.Remove(id);
+            ToDoTask? taskToBeRemoved = _taskRepository.GetById(id);
+            if( taskToBeRemoved == null)
+            {
+                TempData[KEY_ERROR_MESSAGE] = TASK_NOT_FOUND;
+            } else
+            {
+                var result = _taskRepository.Remove(taskToBeRemoved);
+                if( !result )
+                {
+                    TempData[KEY_ERROR_MESSAGE] = REMOVING_PROBLEM;
+                } else
+                {
+                    TempData[KEY_SUCCESS_MESSAGE] = SUCCESSFULLY_DELETED;
+                }
+            }
+
             return RedirectToAction("Index");
         }
         public IActionResult Edit(int id)
         {
             _taskViewModel.ToDo = _taskRepository.GetById(id);
+            if(  _taskViewModel.ToDo == null )
+            {
+                TempData[KEY_ERROR_MESSAGE] = TASK_NOT_FOUND; 
+            }
             return View("Index", _taskViewModel);
         }
+
         [HttpPost]
         public IActionResult Edit(ToDoTask task)
         {
+            _taskViewModel.ToDo = _taskRepository.GetById(task.Id);
+            if( _taskViewModel.ToDo == null)
+            {
+                TempData[KEY_ERROR_MESSAGE] = TASK_NOT_FOUND; 
+            }
             var result = _taskRepository.Update(task);
+            if( !result)
+            {
+                TempData[KEY_ERROR_MESSAGE] = ERROR_TASK_NOT_SAVED;
+                return View(_taskViewModel);
+            }
             _taskViewModel.ToDo = new ToDoTask();
             return RedirectToAction("Index");
         }
